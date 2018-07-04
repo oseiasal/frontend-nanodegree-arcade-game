ce842be29ba76bbe8970713698e6aa20a469621f
Oseias <Oseiasanascimento@hotmail.com>
2018-07-04 11:03:09 -0300

    Fix: Bug speed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function(veloc, x, y) {
+var Enemy = function(x, y, veloc) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = veloc;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -21,6 +22,7 @@ Enemy.prototype.update = function(dt) {
     if (this.x >= 505) {
         this.x = 0;
     }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -29,26 +31,27 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(veloc, x, y) {
+
+var Player = function(x, y, veloc) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.speed = veloc;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    //sprite for char boy
     this.sprite = 'images/char-boy.png';
 };
+
+// This class requires an update(), render() and
+// a handleInput() method.
+
+Player.prototype.update = function() {
+    // void
+}
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-Player.prototype.update = function() {
-    // function not needed right now
-}
 
 Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'left') {
@@ -67,11 +70,13 @@ Player.prototype.handleInput = function(keyPress) {
 };
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var player = new Player(105, 200, 400);
+var player = new Player(200, 400, 105);
 
+// Place all enemy objects in an array called allEnemies
+var enemy = new Enemy(0, Math.random() * 180 + 60, Math.random() * 256);
+allEnemies.push(enemy);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
