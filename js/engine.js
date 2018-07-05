0d6f49d065fc8eacb4df32c0ca948126008323a9
Oseias <Oseiasanascimento@hotmail.com>
2018-07-05 11:06:04 -0300

    Add: Player Render Status Bar #157

--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -117,7 +117,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
@@ -154,6 +154,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        player.renderStatus();
     }
 
     /* This function does nothing but it could have been a good place to
